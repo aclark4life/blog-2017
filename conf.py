6ffa0d423b7553072993ab58f0b0fbb3c1ed421d
Alex Clark <aclark@aclark.net>
2015-11-12 06:28:42 -0500

    Update

--- conf.py
+++ conf.py
@@ -248,7 +248,7 @@ html_theme = 'alabaster'
 html_title = "Alex Clark's Blog"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-# html_short_title = None
+html_short_title = "Alex Clark's Blog"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
